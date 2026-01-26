c0f9fa339282dcfcc3d8902e075d0b0f1b396e9c
pclearn10-lab <pc.learn10@gmail.com>
2026-01-26 13:40:27 +0530

    Update App.py

--- App.py
+++ App.py
@@ -4,7 +4,7 @@ import random
 # Set Page Title & Icon
 st.set_page_config(page_title="Republic Day 2026!", page_icon="🇮🇳")
 St.write("---")
-St.dubheader("created with pride by Pooja Chowdhary")
+St.subheader("created with pride by Pooja Chowdhary")
 # --- DATA BANK ---
 nuggets = [
     "The Indian Constitution is the longest handwritten constitution in the world!",
